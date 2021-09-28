0a4b95bb402f70a975569cf6ae43e896d3226ba0
gus <gustavo_9_rj@hotmail.com>
2021-09-28 02:15:14 +0200

    algo hecho

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const db = require("./app/models");
+const db = require("./models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -16,8 +16,8 @@ db.mongoose
   });
 
 var corsOptions = {
-  origin: "http://localhost:8081"
-};
+    origin: "http://localhost:8081"
+  };
 
 app.use(cors(corsOptions));
 
