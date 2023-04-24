d247d9400ef33b37d0ecd87f7677185f732193f1
Chemchu <gustavo_9_rj@hotmail.com>
2023-04-25 00:39:06 +0200

    feat: rpc crear_producto agregado

---
+++ supabase/sql_functions/add_producto.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION crear_producto(nombre TEXT, 
+    familia TEXT,
+    precioCompra NUMERIC, 
+    precio NUMERIC, 
+    iva NUMERIC, 
+    margen NUMERIC, 
+    cantidad NUMERIC, 
+    proveedorId UUID,
+    codigos_de_barra TEXT[]) 
+RETURNS UUID AS $$
+DECLARE 
+    id UUID;
+    codigo TEXT;
+BEGIN
+    id := uuid_generate_v4();
+    INSERT INTO public.productos (id, nombre, familia, precio_compra, precio_venta, iva, cantidad, proveedor_id, alta, created_at, margen) 
+    VALUES (id, nombre, familia, precioCompra, precio, iva, cantidad, proveedorId, true, current_timestamp, margen);
+    
+    -- Insertar codigos de barra en la tabla de codigos de barra
+    FOREACH codigo IN ARRAY codigos_de_barra
+    LOOP
+        INSERT INTO public.codigos_de_barra (ean_id, producto_id, ean, created_at) 
+        VALUES (uuid_generate_v4(), id, codigo, current_timestamp);
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
