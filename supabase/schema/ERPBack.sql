000c62eaa4ea8200b1031b2dccbe2960dc169ff1
Chemchu <gustavo_9_rj@hotmail.com>
2023-05-27 18:49:04 +0200

    fix: data model being created

--- supabase/schema/ERPBack.sql
+++ supabase/schema/ERPBack.sql
@@ -11,8 +11,8 @@ CREATE TABLE "proveedores" (
   "id" uuid PRIMARY KEY NOT NULL,
   "nombre" text NOT NULL,
   "nif" text UNIQUE NOT NULL,
-  "email_id" uuid,
-  "telefono_id" uuid,
+  "email_id" uuid UNIQUE,
+  "telefono_id" uuid UNIQUE,
   "direccion_id" uuid
 );
 
@@ -27,18 +27,24 @@ CREATE TABLE "pedidos" (
 );
 
 CREATE TABLE "productos_pedidos" (
+  "id" uuid PRIMARY KEY NOT NULL,
   "pedido_id" uuid NOT NULL,
   "producto_id" uuid NOT NULL,
   "precio_compra" numeric NOT NULL,
   "cantidad_pedida" numeric NOT NULL,
-  "cantdad_recibida" numeric NOT NULL,
-  PRIMARY KEY ("pedido_id", "producto_id")
+  "cantdad_recibida" numeric NOT NULL
 );
 
 CREATE TABLE "productos" (
   "id" uuid PRIMARY KEY NOT NULL,
+  "tienda_id" uuid NOT NULL,
   "nombre" text UNIQUE NOT NULL,
   "familia" text NOT NULL,
+  "iva" numeric NOT NULL,
+  "precio_compra" numeric NOT NULL,
+  "precio_venta" numeric NOT NULL,
+  "cantidad" numeric NOT NULL,
+  "alta" boolean NOT NULL DEFAULT true,
   "created_at" timestamptz NOT NULL DEFAULT (now()),
   "updated_at" timestamptz NOT NULL
 );
@@ -48,18 +54,6 @@ CREATE TABLE "codigos_de_barra" (
   "producto_id" uuid NOT NULL,
   "created_at" timestamptz NOT NULL,
   "updated_at" timestamptz NOT NULL
-);
-
-CREATE TABLE "productos_tiendas" (
-  "id" uuid NOT NULL,
-  "producto_id" uuid NOT NULL,
-  "tienda_id" uuid NOT NULL,
-  "iva" numeric NOT NULL,
-  "precio_compra" numeric NOT NULL,
-  "precio_venta" numeric NOT NULL,
-  "cantidad" numeric NOT NULL,
-  "alta" boolean NOT NULL DEFAULT true,
-  PRIMARY KEY ("id", "producto_id", "tienda_id")
 );
 
 CREATE TABLE "productos_mermados" (
@@ -75,24 +69,20 @@ CREATE TABLE "productos_mermados" (
 );
 
 CREATE TABLE "productos_devueltos" (
-  "producto_id" uuid NOT NULL,
-  "venta_id" uuid NOT NULL,
-  "cantidad_devuelta" integer NOT NULL,
+  "id" uuid PRIMARY KEY NOT NULL,
+  "cantidad_devuelta" integer NOT NULL DEFAULT 0,
   "iva" numeric NOT NULL,
   "precio_compra" numeric NOT NULL,
   "precio_venta" numeric NOT NULL,
-  "descuento" numeric NOT NULL DEFAULT 0,
-  PRIMARY KEY ("producto_id", "venta_id")
+  "descuento" numeric NOT NULL DEFAULT 0
 );
 
 CREATE TABLE "ofertas" (
-  "id" uuid NOT NULL,
+  "id" uuid PRIMARY KEY NOT NULL,
   "producto_id" uuid NOT NULL,
-  "tienda_id" uuid NOT NULL,
   "nombre" text NOT NULL,
   "cantidad_para_ofertar" integer NOT NULL,
-  "precio_oferta" numeric NOT NULL,
-  PRIMARY KEY ("id", "producto_id", "tienda_id")
+  "precio_oferta" numeric NOT NULL
 );
 
 CREATE TABLE "tiendas" (
@@ -161,20 +151,19 @@ CREATE TABLE "emails" (
 );
 
 CREATE TABLE "productos_vendidos" (
+  "id" uuid PRIMARY KEY NOT NULL,
   "producto_id" uuid NOT NULL,
   "venta_id" uuid NOT NULL,
   "cantidad_vendida" integer NOT NULL,
   "iva" numeric NOT NULL,
   "precio_compra" numeric NOT NULL,
   "precio_venta" numeric NOT NULL,
-  "descuento" numeric NOT NULL DEFAULT 0,
-  PRIMARY KEY ("producto_id", "venta_id")
+  "descuento" numeric NOT NULL DEFAULT 0
 );
 
 CREATE TABLE "ofertas_aplicadas" (
-  "id" uuid PRIMARY KEY NOT NULL,
+  "oferta_aplicada_id" uuid PRIMARY KEY NOT NULL,
   "producto_vendido_id" uuid NOT NULL,
-  "venta_id" uuid NOT NULL,
   "nombre" text NOT NULL,
   "cantidad_para_ofertar" integer NOT NULL,
   "precio_oferta" numeric NOT NULL,
@@ -182,13 +171,12 @@ CREATE TABLE "ofertas_aplicadas" (
 );
 
 CREATE TABLE "ofertas_devueltas" (
-  "id" uuid PRIMARY KEY NOT NULL,
+  "oferta_devuelta_id" uuid PRIMARY KEY NOT NULL,
   "producto_devuelto_id" uuid NOT NULL,
-  "venta_id" uuid NOT NULL,
   "nombre" text NOT NULL,
   "cantidad_para_ofertar" integer NOT NULL,
   "precio_oferta" numeric NOT NULL,
-  "cantidad_ofertas_devueltas" integer NOT NULL DEFAULT 1
+  "cantidad_ofertas_devueltas" integer NOT NULL DEFAULT 0
 );
 
 CREATE TABLE "ventas" (
@@ -217,17 +205,7 @@ ALTER TABLE "pedidos" ADD FOREIGN KEY ("proveedor_id") REFERENCES "proveedores" 
 
 ALTER TABLE "productos_pedidos" ADD FOREIGN KEY ("pedido_id") REFERENCES "pedidos" ("id");
 
-ALTER TABLE "productos_pedidos" ADD FOREIGN KEY ("producto_id") REFERENCES "productos_tiendas" ("producto_id");
-
 ALTER TABLE "codigos_de_barra" ADD FOREIGN KEY ("producto_id") REFERENCES "productos" ("id");
-
-ALTER TABLE "ofertas" ADD FOREIGN KEY ("producto_id") REFERENCES "productos_tiendas" ("producto_id");
-
-ALTER TABLE "ofertas" ADD FOREIGN KEY ("tienda_id") REFERENCES "productos_tiendas" ("tienda_id");
-
-ALTER TABLE "productos_tiendas" ADD FOREIGN KEY ("producto_id") REFERENCES "productos" ("id");
-
-ALTER TABLE "productos_tiendas" ADD FOREIGN KEY ("tienda_id") REFERENCES "tiendas" ("id");
 
 ALTER TABLE "terminales_de_ventas" ADD FOREIGN KEY ("tienda_id") REFERENCES "tiendas" ("id");
 
@@ -237,21 +215,11 @@ ALTER TABLE "telefonos" ADD FOREIGN KEY ("entidad_id") REFERENCES "tiendas" ("id
 
 ALTER TABLE "emails" ADD FOREIGN KEY ("entidad_id") REFERENCES "tiendas" ("id");
 
-ALTER TABLE "ofertas_devueltas" ADD FOREIGN KEY ("producto_devuelto_id") REFERENCES "productos_devueltos" ("producto_id");
-
-ALTER TABLE "ofertas_devueltas" ADD FOREIGN KEY ("venta_id") REFERENCES "productos_devueltos" ("venta_id");
-
-ALTER TABLE "ofertas_aplicadas" ADD FOREIGN KEY ("producto_vendido_id") REFERENCES "productos_vendidos" ("producto_id");
-
-ALTER TABLE "ofertas_aplicadas" ADD FOREIGN KEY ("venta_id") REFERENCES "productos_vendidos" ("venta_id");
-
 ALTER TABLE "cierres" ADD FOREIGN KEY ("terminal_id") REFERENCES "terminales_de_ventas" ("id");
 
 ALTER TABLE "ventas" ADD FOREIGN KEY ("cierre_id") REFERENCES "cierres" ("id");
 
 ALTER TABLE "productos_vendidos" ADD FOREIGN KEY ("venta_id") REFERENCES "ventas" ("id");
-
-ALTER TABLE "productos_vendidos" ADD FOREIGN KEY ("producto_id") REFERENCES "productos_tiendas" ("producto_id");
 
 ALTER TABLE "emails" ADD FOREIGN KEY ("id") REFERENCES "empleados" ("email_id");
 
@@ -261,16 +229,6 @@ ALTER TABLE "empleados_tiendas" ADD FOREIGN KEY ("empleado_id") REFERENCES "empl
 
 ALTER TABLE "empleados_tiendas" ADD FOREIGN KEY ("tienda_id") REFERENCES "tiendas" ("id");
 
-ALTER TABLE "productos_mermados" ADD FOREIGN KEY ("producto_id") REFERENCES "productos_tiendas" ("producto_id");
-
-ALTER TABLE "emails" ADD FOREIGN KEY ("id") REFERENCES "proveedores" ("email_id");
-
-ALTER TABLE "telefonos" ADD FOREIGN KEY ("id") REFERENCES "proveedores" ("telefono_id");
-
-ALTER TABLE "productos_devueltos" ADD FOREIGN KEY ("producto_id") REFERENCES "productos_vendidos" ("producto_id");
-
-ALTER TABLE "productos_devueltos" ADD FOREIGN KEY ("venta_id") REFERENCES "ventas" ("id");
-
 ALTER TABLE "tiendas" ADD FOREIGN KEY ("empresa_id") REFERENCES "empresas" ("id");
 
 ALTER TABLE "inventarios" ADD FOREIGN KEY ("producto_id") REFERENCES "productos" ("id");
@@ -278,3 +236,23 @@ ALTER TABLE "inventarios" ADD FOREIGN KEY ("producto_id") REFERENCES "productos"
 ALTER TABLE "inventarios" ADD FOREIGN KEY ("tienda_id") REFERENCES "tiendas" ("id");
 
 ALTER TABLE "pedidos" ADD FOREIGN KEY ("tienda_id") REFERENCES "tiendas" ("id");
+
+ALTER TABLE "productos_devueltos" ADD FOREIGN KEY ("id") REFERENCES "productos_vendidos" ("id");
+
+ALTER TABLE "ofertas_devueltas" ADD FOREIGN KEY ("producto_devuelto_id") REFERENCES "productos_devueltos" ("id");
+
+ALTER TABLE "ofertas_aplicadas" ADD FOREIGN KEY ("producto_vendido_id") REFERENCES "productos_vendidos" ("id");
+
+ALTER TABLE "productos" ADD FOREIGN KEY ("tienda_id") REFERENCES "tiendas" ("id");
+
+ALTER TABLE "productos_pedidos" ADD FOREIGN KEY ("producto_id") REFERENCES "productos" ("id");
+
+ALTER TABLE "ofertas" ADD FOREIGN KEY ("producto_id") REFERENCES "productos" ("id");
+
+ALTER TABLE "productos_mermados" ADD FOREIGN KEY ("producto_id") REFERENCES "productos" ("id");
+
+ALTER TABLE "productos_vendidos" ADD FOREIGN KEY ("producto_id") REFERENCES "productos" ("id");
+
+ALTER TABLE "emails" ADD FOREIGN KEY ("id") REFERENCES "proveedores" ("email_id");
+
+ALTER TABLE "telefonos" ADD FOREIGN KEY ("id") REFERENCES "proveedores" ("telefono_id");
